e79bb62bb9afac867a3b2af6d5398ced0ccd8921
nhglong <its.nhglong@gmail.com>
2026-01-19 21:29:12 +0700

    drivers: Wire-up fingerprint from A515FXXU4DUB1
    * Enable ENABLE_SENSORS_FPRINT_SECURE cflags globally
    * Wire-up SENSORS_FP_SPI_NUMBER and SENSORS_FP_USI_NUMBER

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -42,6 +42,10 @@
 /* maximum number of the memory resources */
 #define	SAMSUNG_PINCTRL_NUM_RESOURCES	2
 
+#if defined(ENABLE_SENSORS_FPRINT_SECURE)
+extern int fps_resume_set(void);
+#endif
+
 /* list of all possible config options supported */
 static struct pin_config {
 	const char *property;
@@ -1317,6 +1321,10 @@ static void samsung_pinctrl_resume_dev(struct samsung_pinctrl_drv_data *drvdata)
 		/* Registers without a powerdown config aren't lost */
 		if (!widths[PINCFG_TYPE_CON_PDN])
 			continue;
+
+#if defined(ENABLE_SENSORS_FPRINT_SECURE)
+		fps_resume_set();
+#endif
 
 		if (widths[PINCFG_TYPE_FUNC] * bank->nr_pins > 32) {
 			/* Some banks have two config registers */
--- drivers/spi/spi-s3c64xx.c
+++ drivers/spi/spi-s3c64xx.c
@@ -1221,6 +1221,13 @@ static int s3c64xx_spi_setup(struct spi_device *spi)
 		return -ENODEV;
 	}
 
+#ifdef ENABLE_SENSORS_FPRINT_SECURE
+	if (sdd->port_id == CONFIG_SENSORS_FP_SPI_NUMBER) {
+		dev_info(&spi->dev,
+				"spi configuration for secure channel is skipped(FP)\n");
+		return 0;
+	}
+#endif
 	if (!spi_get_ctldata(spi)) {
 		if(cs->line != 0) {
 			err = gpio_request_one(cs->line, GPIOF_OUT_INIT_HIGH,
@@ -1391,6 +1398,10 @@ static void exynos_usi_init(struct s3c64xx_spi_driver_data *sdd)
 	 * Due to this feature, the USI_RESET must be cleared (set as '0')
 	 * before transaction starts.
 	 */
+#ifdef ENABLE_SENSORS_FPRINT_SECURE
+	if (sdd->port_id == CONFIG_SENSORS_FP_SPI_NUMBER)
+		return;
+#endif
 	writel(USI_RESET, regs + USI_CON);
 }
 
@@ -1400,6 +1411,10 @@ static void s3c64xx_spi_hwinit(struct s3c64xx_spi_driver_data *sdd, int channel)
 	void __iomem *regs = sdd->regs;
 	unsigned int val;
 
+#ifdef ENABLE_SENSORS_FPRINT_SECURE
+	if (channel == CONFIG_SENSORS_FP_SPI_NUMBER)
+		return;
+#endif
 	sdd->cur_speed = 0;
 
 	writel(S3C64XX_SPI_SLAVE_SIG_INACT, sdd->regs + S3C64XX_SPI_SLAVE_SEL);
@@ -1757,9 +1772,15 @@ static int s3c64xx_spi_probe(struct platform_device *pdev)
 		goto err3;
 	}
 
-	writel(S3C64XX_SPI_INT_RX_OVERRUN_EN | S3C64XX_SPI_INT_RX_UNDERRUN_EN |
-	       S3C64XX_SPI_INT_TX_OVERRUN_EN | S3C64XX_SPI_INT_TX_UNDERRUN_EN,
-	       sdd->regs + S3C64XX_SPI_INT_EN);
+	if (1
+#ifdef ENABLE_SENSORS_FPRINT_SECURE
+		&& (sdd->port_id != CONFIG_SENSORS_FP_SPI_NUMBER)
+#endif
+	) {
+		writel(S3C64XX_SPI_INT_RX_OVERRUN_EN | S3C64XX_SPI_INT_RX_UNDERRUN_EN |
+			   S3C64XX_SPI_INT_TX_OVERRUN_EN | S3C64XX_SPI_INT_TX_UNDERRUN_EN,
+			   sdd->regs + S3C64XX_SPI_INT_EN);
+	}
 
 #ifdef CONFIG_PM
 	pm_runtime_mark_last_busy(&pdev->dev);
